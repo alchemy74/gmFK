d5777c461f33943f4fa54ca8fc2c6be4fd647f16
Norman <normanke@gamania.com>
2017-01-04 16:27:02 +0800

    Add data access temp

---
+++ gmfucker/gmFucker.DataAccess/Adapters/DataAdapterBase.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gmFucker.DataAccess.Adapters
+{
+
+    /// <summary>
+    /// 資料庫連接的基底型別
+    /// </summary>
+    public abstract class DataAdapterBase
+    {
+        /// <summary>
+        /// 執行資料庫預存程序
+        /// </summary>
+        /// <param name="storedProcedureName">資料庫預存程序名稱</param>
+        /// <param name="sqlParameters">資料庫預存程序參數</param>
+        /// <returns>受影響的資料筆數</returns>
+        protected int executeStoredProcedure(string storedProcedureName, params SqlParameter[] sqlParameters)
+        {
+            int affectedDataRowCount = 0;
+            using (SqlConnection connection = new SqlConnection(this.ConnectString))
+            {
+                SqlCommand command = new SqlCommand(storedProcedureName, connection);
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                if (sqlParameters != null)
+                {
+                    foreach (SqlParameter sqlParameter in sqlParameters)
+                    {
+                        command.Parameters.Add(sqlParameter);
+                    }
+                }
+                connection.Open();
+                affectedDataRowCount = command.ExecuteNonQuery();
+                connection.Close();
+            }
+            return affectedDataRowCount;
+        }
+
+        /// <summary>
+        /// 執行資料庫預存程序,並以字典型別作為預存程序參數進行資料傳輸
+        /// </summary>
+        /// <param name="storedProcedureName">資料庫預存程序名稱</param>
+        /// <param name="sqlParameters">資料庫預存程序參數</param>
+        /// <returns>受影響的資料筆數</returns>
+        protected int executeStoredProcedure(string storedProcedureName, Dictionary<string, object> sqlParameters = null)
+        {
+            int affectedDataRowCount = 0;
+            using (SqlConnection connection = new SqlConnection(this.ConnectString))
+            {
+                SqlCommand command = new SqlCommand(storedProcedureName, connection);
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                if (sqlParameters != null)
+                {
+                    foreach (string key in sqlParameters.Keys)
+                    {
+                        command.Parameters.AddWithValue(key, sqlParameters[key]);
+                    }
+                }
+                connection.Open();
+                affectedDataRowCount = command.ExecuteNonQuery();
+                connection.Close();
+            }
+            return affectedDataRowCount;
+        }
+
+        /// <summary>
+        /// 執行資料庫預存程序並回傳資料
+        /// </summary>
+        /// <typeparam name="T">回傳資料的型別</typeparam>
+        /// <param name="storedProcedureName">資料庫預存程序名稱</param>
+        /// <param name="sqlParameters">資料庫預存程序參數</param>
+        /// <returns>資料實體集合</returns>
+        protected IEnumerable<T> executeStoredProcedure_WithReturnValue<T>(string storedProcedureName, Func<SqlDataReader, T> getEntityData, Dictionary<string, object> sqlParameters = null)
+        {
+            using (SqlConnection connection = new SqlConnection(this.ConnectString))
+            {
+                using (SqlCommand command = new SqlCommand(storedProcedureName, connection))
+                {
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    if (sqlParameters != null)
+                    {
+                        foreach (string key in sqlParameters.Keys)
+                        {
+                            command.Parameters.AddWithValue(key, sqlParameters[key]);
+                        }
+                    }
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        yield return getEntityData(reader);
+                    }
+                    connection.Close();
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// 資料庫連線字串
+        /// </summary>
+        private string ConnectString { get; } = ConfigurationManager.ConnectionStrings[" Your Custom DataBase Name "].ToString();
+    }
+}
---
+++ gmfucker/gmFucker.DataAccess/Base/StoreProcedures.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gmFucker.DataAccess.Base
+{
+
+    /// <summary>
+    /// 資料庫預存程序(範例)
+    /// </summary>
+    internal class StoreProcedures
+    {
+        /// <summary>
+        /// 第三方支付的預存程序
+        /// </summary>
+        internal struct OrderWithThirdPartyPay
+        {
+            /// <summary>
+            /// 新增一筆第三方支付交易記錄資料的資料庫預存程序
+            /// </summary>
+            internal const string OrderWithThirdPartyPay_Add = "usp_OrderWithThirdPartyPay_Add";
+
+            /// <summary>
+            /// 依流水號ID取得第三方支付交易記錄的資料庫預存程序
+            /// </summary>
+            internal const string OrderWithThirdPartyPay_Get = "usp_OrderWithThirdPartyPay_Get";
+
+            /// <summary>
+            /// 依訂單標號取得第三方支付交易記錄資料集合的資料庫預存程序
+            /// </summary>
+            internal const string OrderWithThirdPartyPay_GetByFormID = "usp_OrderWithThirdPartyPay_GetByFormID";
+
+            /// <summary>
+            /// 依第三方支付訂單編號取得第三方支付交易記錄資料集合的資料庫預存程序
+            /// </summary>
+            internal const string OrderWithThirdPartyPay_GetByThirdPartyPayOrderNumber = "usp_OrderWithThirdPartyPay_GetByThirdPartyPayOrderNumber";
+
+            /// <summary>
+            /// 依付款狀態取得第三方支付交易記錄資料集合的資料庫預存程序
+            /// </summary>
+            internal const string OrderWithThirdPartyPay_GetByStatus = "usp_OrderWithThirdPartyPay_GetByStatus";
+
+            /// <summary>
+            /// 依第三方支付類型取得第三方支付交易記錄資料集合的資料庫預存程序
+            /// </summary>
+            internal const string OrderWithThirdPartyPay_GetByThirdPartyPayType = "usp_OrderWithThirdPartyPay_GetByThirdPartyPayType";
+
+            /// <summary>
+            /// 修改一筆第三方交易紀錄資料的資料庫預存程序
+            /// </summary>
+            internal const string OrderWithThirdPartyPay_Update = "usp_OrderWithThirdPartyPay_Update";
+        }
+
+        internal struct SiteSettings
+        {
+            /// <summary>
+            /// 新增一筆頁面設定項目的資料庫預存程序
+            /// </summary>
+            internal const string SiteSettings_Add = "usp_SiteSettings_Add";
+
+            /// <summary>
+            /// 刪除一筆頁面設定項目的資料庫預存程序
+            /// </summary>
+            internal const string SiteSettings_Delete = "usp_SiteSettings_Delete";
+
+            /// <summary>
+            /// 依ID取得一筆頁面設定項目的資料庫預存程序
+            /// </summary>
+            internal const string SiteSettings_Get = "usp_SiteSettings_Get";
+
+            /// <summary>
+            /// 依ID修改一筆頁面設定項目的資料庫預存程序
+            /// </summary>
+            internal const string SiteSettings_Update = "usp_SiteSettings_Update";
+        }
+    }
+}
